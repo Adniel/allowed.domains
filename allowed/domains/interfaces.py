3176b1c8501d1452dd6e83a202cc156e73fccd89
Daniel Grindelid <daniel.grindelid@gmail.com>
2013-11-06 16:05:24 +0100

    Removed files

--- allowed/domains/interfaces.py
+++ allowed/domains/interfaces.py
@@ -9,7 +9,6 @@ class IAllowedDomainsSettings(Interface):
 
     """ Allowed domains
     """
-
     allowed_domains = schema.List(value_type=schema.TextLine(),
                                   title=_(u'Allowed domains'),
                                   description=_(u'List of allowed domains when registering users. One domain per line.'),
